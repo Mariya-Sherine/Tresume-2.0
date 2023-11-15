2f4bae730a6ecf43a8809642a7b3d68271e82356
Mariya-Sherine <141424695+Mariya-Sherine@users.noreply.github.com>
2023-11-16 00:48:20 +0530

    Update_AllJobPosting

--- src/app/all-job-postings/all-job-postings.component.ts
+++ src/app/all-job-postings/all-job-postings.component.ts
@@ -1,19 +1,18 @@
-import { AllJobPostingsService } from './all-job-postings.service';
-import { MatDialog } from '@angular/material/dialog';
-import { ConfirmationPopupComponent } from '../confirmation-popup/confirmation-popup.component';
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { CookieService } from 'ngx-cookie-service';
 import { MessageService } from 'primeng/api';
 import { Router } from '@angular/router';
+import { AllJobPostingsService } from './all-job-postings.service';
 
 @Component({
   selector: 'app-all-job-postings',
   templateUrl: './all-job-postings.component.html',
   styleUrls: ['./all-job-postings.component.scss'],
-  providers: [AllJobPostingsService, CookieService,MessageService],
+  providers: [AllJobPostingsService, CookieService, MessageService],
 })
-export class AllJobPostingsComponent implements OnInit{
+export class AllJobPostingsComponent implements OnInit {
   OrgID:string = '';
   JobID:string = '';
   TraineeID:string = '';
@@ -27,25 +26,59 @@ ngOnInit(): void {
   this.TraineeID = this.cookieService.get('TraineeID');
   this.fetchjobpostinglist();
 }
-  constructor(private dialog: MatDialog,private cookieService: CookieService, private service:AllJobPostingsService,private messageService: MessageService) {}
 
+  insertForm: FormGroup;
 
-ngOnChanges(): void{
-  // this.fetchuserlist();
-}
+  constructor(private dialog: MatDialog, private cookieService: CookieService, private service: AllJobPostingsService, private messageService: MessageService, private fb: FormBuilder,
+  ) {
+    this.insertForm = this.fb.group({
+      jobTitle: ['', Validators.required],
+      company: ['', Validators.required],
+      location: ['', Validators.required],
+      payRate: [0, Validators.min(0)],
+      newApplicants: [0, Validators.min(0)],
+      totalApplicants: [0, Validators.min(0)],
+      postedOn: ['', Validators.required], 
+      postedBy: ['', Validators.required],
+      jobType: ['', Validators.required]
+    });
+  }
+  fetchjobpostinglist(){
+    let Req = {
+      OrgID: this.OrgID,
+    };
+    this.service.getJobPostingList(Req).subscribe((x: any) => {
+      this.jobs = x.result;
+      this.noResultsFound = this.jobs.length === 0;
+    });
+  }
+  
+  jobOptions = ['My Jobs', 'Assigned Jobs', 'All Jobs'];
 
+  insertJob() {
+    if (this.insertForm.valid) {
+      const newJob = this.insertForm.value;
 
-fetchjobpostinglist(){
-  let Req = {
-    OrgID: this.OrgID,
-  };
-  this.service.getJobPostingList(Req).subscribe((x: any) => {
-    this.jobs = x.result;
-    this.noResultsFound = this.jobs.length === 0;
-  });
-}
-
-jobOptions = ['My Jobs', 'Assigned Jobs', 'All Jobs'];
-
-
+      this.service.insertJobPosting(newJob).subscribe(
+        (response) => {
+          this.fetchjobpostinglist();
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Job inserted successfully',
+          });
+        },
+        (error) => {
+          console.error('Error inserting job:', error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Failed to insert job',
+          });
+        }
+      );
+    } else {
+      
+    }
+  }
 }
--- src/app/all-job-postings/all-job-postings.service.ts
+++ src/app/all-job-postings/all-job-postings.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpEvent, HttpParams, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
 @Injectable()
-
 export class AllJobPostingsService {
-    public endpoint = environment.apiUrl;
-    // public endpoint = 'https://alpha.tresume.us/TresumeAPI/';
-    // public endpoint = 'http://localhost:3000/';
+  public endpoint = environment.apiUrl;
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
+  createTimesheet(data: any) {
+    const url = `${this.endpoint}/'https://beta.tresume.us/TresumeAPI'`; 
+    return this.http.post(url, data);
+  }
 
-    // deleteJobPosting(request: any): Observable<ResponseDetails> {
-    //     return this.http.post<ResponseDetails>(this.endpoint + 'deleteJobPosting', request);
-    // }
+  getJobPostingList(request: any): Observable<ResponseDetails> {
+    return this.http.post<ResponseDetails>(this.endpoint + 'getJobPostingList', request);
+  }
 
-    getJobPostingList(request: any): Observable<ResponseDetails> {
-        return this.http.post<ResponseDetails>(this.endpoint + 'getJobPostingList', request);
-    }
+  insertJobPosting(jobDetails: any): Observable<ResponseDetails> {
+    return this.http.post<ResponseDetails>(this.endpoint + 'insertJobPosting', jobDetails);
+  }
 }
+
 export interface ResponseDetails {
-    flag?: any;
-    result?: any;
+  flag?: any;
+  result?: any;
 }
